6c43c66c6eb9c329ded24510138dea74afcc9603
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2025-02-26 11:40:28 +0100

    add exo 5

---
+++ lib/exo5a.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Exo5a extends StatelessWidget {
+  const Exo5a({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GridView.count(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          padding: const EdgeInsets.all(10),
+          children: List.generate(
+            9,
+            (index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Center(
+                child: Text(
+                  'Item $index',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+          ),
+        )
+    );
+}
+}
--- lib/exolist.dart
+++ lib/exolist.dart
@@ -2,6 +2,7 @@ import 'exo2b.dart';
 import 'exo.dart';
 import 'exo2.dart';
 import 'exo4.dart';
+import 'exo5a.dart';
 
 final exerciseList = [
   Exercice(
@@ -10,4 +11,7 @@ final exerciseList = [
       name: 'Exercice 2b',
       description: ' Animated Rotate & Scale Image',
       page: Exo2b()),
+  Exercice(
+      name: 'Exercice 4', description: 'ClipRect & FittedBox', page: Exo4()),
+  Exercice(name: 'Exercice 5', description: 'Display a grid', page: Exo5a())
 ];
